f379b2d67743ed649764a084233f335e46b72fab
SCollins <jaybuckco@gmail.com>
2022-07-20 13:21:23 +0000

    Add starting variables and objects

---
+++ run.py
@@ -0,0 +1,12 @@
+"""
+Global variables.
+Username empty string to be filled by user input.
+Pick up inventory list to be added to by user during story.
+Booleans to be changed by user decisions during story.
+"""
+username = ""
+pick_ups = []
+window_closed = False
+door_open = False
+stool_out = False
+shadow_delayed = False
